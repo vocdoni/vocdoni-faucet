3fc31bbd7644c982c594bf01d4f84344ed2548fa
Jordi Pinyana <jordi.pinyana.paga@gmail.com>
2022-10-13 19:13:46 +0200

    add faucet api

--- api/api.go
+++ api/api.go
@@ -1,150 +1,142 @@
-package restAPI
+package api
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
-	"path/filepath"
 	"strings"
 
-	"go.vocdoni.io/dvote/db"
-	"go.vocdoni.io/dvote/db/metadb"
+	"github.com/ethereum/go-ethereum/common"
 	"go.vocdoni.io/dvote/httprouter"
 	"go.vocdoni.io/dvote/httprouter/bearerstdapi"
-	"go.vocdoni.io/dvote/metrics"
+	"go.vocdoni.io/dvote/log"
 	"go.vocdoni.io/dvote/types"
+	"go.vocdoni.io/proto/build/go/models"
 	"go.vocdoni.io/vocdoni-faucet/faucet"
 )
 
-type FaucetNetworks int
-
 const (
-	VocdoniFaucetHandlerName                = "vocdoni"
-	EVMFaucetHandlerName                    = "evm"
-	MaxPageSize                             = 10
-	FaucetNetworksEthereum   FaucetNetworks = iota
-	FaucetNetworksVocdoniDev
-	FaucetNetworksVocdoniAzeno
-	FaucetNetworksGoerli
-	FaucetNetworksSepolia
-	FaucetNetworksMatic
-	FaucetNetworksMumbai
-	FaucetNetworksGnosisChain
+	EVM     = "evm"
+	Vocdoni = "vocdoni"
 )
 
-var (
-	SupportedFaucetNetworksMap = map[string]FaucetNetworks{
-		"ethereum":     FaucetNetworksEthereum,
-		"vocdoniDev":   FaucetNetworksVocdoniDev,
-		"vocdoniAzeno": FaucetNetworksVocdoniAzeno,
-		"goerli":       FaucetNetworksGoerli,
-		"sepolia":      FaucetNetworksSepolia,
-		"matic":        FaucetNetworksMatic,
-		"mumbai":       FaucetNetworksMumbai,
-		"gnosisChain":  FaucetNetworksGnosisChain,
-	}
-)
+// FaucetRequestData represents the data of a faucet request
+type FaucetRequestData struct {
+	// Network represents one of the supported faucet networks (evm or vocdoni)
+	Network string `json:"network"`
+	// From represents the address for the faucet to send tokens
+	From types.HexBytes `json:"from"`
+}
 
-// RestAPI is the URL based REST API supporting bearer authentication.
-type RestAPI struct {
-	PrivateCalls uint64
-	PublicCalls  uint64
-	BaseRoute    string
+// FaucetResponse represents the message on the response of a faucet request
+type FaucetResponse struct {
+	// FaucetPackage is the Vocdoni faucet package
+	FaucetPackage *models.FaucetPackage `json:"faucetPackage,omitempty"`
+	// TxHash is the EVM tx hash
+	TxHash types.HexBytes `json:"txHash,omitempty"`
+}
 
-	db            db.Database
+// API is the URL based API supporting bearer authentication.
+type API struct {
+	baseRoute string
+
 	router        *httprouter.HTTProuter
 	api           *bearerstdapi.BearerStandardAPI
 	evmFaucet     *faucet.EVM
 	vocdoniFaucet *faucet.Vocdoni
-	//lint:ignore U1000 unused
-	metricsagent *metrics.Agent
 }
 
-// NewRestAPI creates a new instance of the RestAPI.  Attach must be called next.
-func NewRestAPI(router *httprouter.HTTProuter, baseRoute, dataDir string) (*RestAPI, error) {
+// NewAPI returns a new instance of the API
+func NewAPI() *API {
+	return &API{}
+}
+
+// Init initianizes an API instance
+func (a *API) Init(router *httprouter.HTTProuter, baseRoute string, vfaucet *faucet.Vocdoni, efaucet *faucet.EVM) error {
 	if router == nil {
-		return nil, fmt.Errorf("httprouter is nil")
+		return fmt.Errorf("httprouter is nil")
 	}
+	a.router = router
 	if len(baseRoute) == 0 || baseRoute[0] != '/' {
-		return nil, fmt.Errorf("invalid base route (%s), it must start with /", baseRoute)
+		return fmt.Errorf("invalid base route (%s), it must start with /", baseRoute)
 	}
 	// remove trailing slash
 	if len(baseRoute) > 1 {
 		baseRoute = strings.TrimSuffix(baseRoute, "/")
 	}
-	restAPI := RestAPI{
-		BaseRoute: baseRoute,
-		router:    router,
-	}
+	a.baseRoute = baseRoute
+	// bearer token api
 	var err error
-	restAPI.api, err = bearerstdapi.NewBearerStandardAPI(router, baseRoute)
-	if err != nil {
-		return nil, err
+	if a.api, err = bearerstdapi.NewBearerStandardAPI(a.router, a.baseRoute); err != nil {
+		return err
 	}
-	// create local key value database
-	restAPI.db, err = metadb.New(db.TypePebble, filepath.Join(dataDir, "restAPI"))
-	if err != nil {
-		return nil, err
+	// attach faucet modules
+	a.attach(vfaucet, efaucet)
+	// enable handlers
+	if err := a.enableFaucetHandlers(); err != nil {
+		return fmt.Errorf("cannot enable handlers %w", err)
 	}
-	// register endpoint
-	if err := restAPI.api.RegisterMethod(
+	return nil
+}
+
+func (a *API) enableFaucetHandlers() error {
+	if err := a.api.RegisterMethod(
 		"/faucet",
 		"POST",
 		bearerstdapi.MethodAccessTypePublic,
-		restAPI.faucetHandler,
+		a.faucetHandler,
 	); err != nil {
-		return nil, err
+		return err
 	}
-
-	return &restAPI, nil
+	return nil
 }
 
-// Attach takes a list of modules which are used by the handlers in order to interact with the system.
-// Attach must be called before EnableHandlers.
-func (u *RestAPI) Attach(vocdoniFaucet *faucet.Vocdoni, EVMFaucet *faucet.EVM, metricsAgent *metrics.Agent) {
-	u.vocdoniFaucet = vocdoniFaucet
-	u.evmFaucet = EVMFaucet
-	u.metricsagent = metricsAgent
-}
-
-type FaucetRequest struct {
-	Network string         `json:"network"`
-	From    types.HexBytes `json:"from"`
+// attach takes a list of modules which are used by the handlers in order to interact with the system.
+// Attach must be called before enableHandlers.
+func (a *API) attach(vocdoniFaucet *faucet.Vocdoni, EVMFaucet *faucet.EVM) {
+	a.vocdoniFaucet = vocdoniFaucet
+	a.evmFaucet = EVMFaucet
 }
 
 // request funds to the faucet
-func (u *RestAPI) faucetHandler(msg *bearerstdapi.BearerStandardAPIdata, ctx *httprouter.HTTPContext) error {
-	req := &FaucetRequest{}
+func (a *API) faucetHandler(msg *bearerstdapi.BearerStandardAPIdata, ctx *httprouter.HTTPContext) error {
+	req := &FaucetRequestData{}
 	if err := json.Unmarshal(msg.Data, req); err != nil {
 		return err
 	}
+	fromAddress := common.BytesToAddress(req.From)
+	log.Infof(`request: { "network": %s, "from": %s }`, req.Network, fromAddress)
 
-	if _, ok := SupportedFaucetNetworksMap[req.Network]; !ok {
-		return fmt.Errorf("%s", "unsupported network")
-	}
-
-	switch SupportedFaucetNetworksMap[req.Network] {
-	// executar evm
-	case FaucetNetworksEthereum,
-		FaucetNetworksGoerli,
-		FaucetNetworksSepolia,
-		FaucetNetworksMatic,
-		FaucetNetworksMumbai,
-		FaucetNetworksGnosisChain:
-		txhash, err := u.evmFaucet.Send()
+	switch req.Network {
+	// evm
+	case EVM:
+		data, err := a.evmFaucet.SendTokens(context.Background(), fromAddress)
 		if err != nil {
 			return fmt.Errorf("error sending evm tokens: %s", err)
 		}
-		return ctx.Send(txhash, bearerstdapi.HTTPstatusCodeOK)
-	case FaucetNetworksVocdoniDev, FaucetNetworksVocdoniAzeno:
-		faucetPackage, err := u.vocdoniFaucet.Send()
+		resp := &FaucetResponse{
+			TxHash: types.HexBytes(data.Bytes()),
+		}
+		msg, err := json.Marshal(resp)
+		if err != nil {
+			return err
+		}
+		return ctx.Send(msg, bearerstdapi.HTTPstatusCodeOK)
+	// vocdoni
+	case Vocdoni:
+		faucetPackage, err := a.vocdoniFaucet.GenerateFaucetPackage(fromAddress)
 		if err != nil {
 			return fmt.Errorf("error sending evm tokens: %s", err)
 		}
-		return ctx.Send(faucetPackage, bearerstdapi.HTTPstatusCodeOK)
-	// crear faucet pkg
+		resp := &FaucetResponse{
+			FaucetPackage: faucetPackage,
+		}
+		msg, err := json.Marshal(resp)
+		if err != nil {
+			return err
+		}
+		return ctx.Send(msg, bearerstdapi.HTTPstatusCodeOK)
 	default:
 		return fmt.Errorf("%s", "unsupported network")
 	}
-
-	return ctx.Send(data, bearerstdapi.HTTPstatusCodeOK)
 }
