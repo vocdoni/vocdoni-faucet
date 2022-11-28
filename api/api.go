125a3257613c9291d0cd93da5dd396f0d1422643
p4u <pau@dabax.net>
2022-11-28 16:04:09 +0100

    increase from 100 to 130 the max line size for linter
    100 is too short and makes code less redeable
    
    Signed-off-by: p4u <pau@dabax.net>

--- api/api.go
+++ api/api.go
@@ -230,8 +230,7 @@ func (a *API) evmFaucetHandler(ctx *httprouter.HTTPContext,
 }
 
 // request vocdoni funds to the faucet
-func (a *API) vocdoniFaucetHandler(ctx *httprouter.HTTPContext,
-	network faucet.FaucetNetworks, from common.Address) error {
+func (a *API) vocdoniFaucetHandler(ctx *httprouter.HTTPContext, network faucet.FaucetNetworks, from common.Address) error {
 	if faucet.VocdoniSupportedFaucetNetworksMap[a.vocdoniFaucet.Network()] != network {
 		return fmt.Errorf("unavailable network")
 	}
